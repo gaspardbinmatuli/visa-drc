6c84cbeace1bf1d142e9a5847e291cce11a4225f
gaspardbinmatuli <gaspardbinmatuli@gmail.com>
2021-11-02 14:00:47 +0100

    integration des pages statiques

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,8 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about-us/', views.about_us, name='about-us'),
+    path('contact-us/', views.contact_us, name='contact-us'),
+    path('pricing/', views.pricing, name='pricing')
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home/pages/index.html')
+
+def about_us(request):
+    return render(request, 'home/pages/about-us.html')
+
+def contact_us(request):
+    return render(request, 'home/pages/contact-us.html')
+
+def pricing(request):
+    return render(request, 'home/pages/pricing.html')
